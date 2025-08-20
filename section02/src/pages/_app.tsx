680b11040ea107dcd015a247ce5558c7c6d5a008
Cheolwoo Nam <545i@naver.com>
2025-08-20 20:34:34 +0900

    Commit ~section03 study

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,21 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import GlobalLayout from "@/components/global-layout";
+import { ReactNode } from "react";
+import { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-  }
-  return <>
-    <header>
-      <Link href={"/"}>index</Link>&nbsp;
-      <Link href={"/search"}>search</Link>&nbsp;
-      <div>
-        <button onClick={onClickButton}>/test 페이지로 이동</button>
-      </div>
-    </header>
-    <Component {...pageProps} />
-  </>;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+export default function App(
+  { Component,
+    pageProps,
+  }: AppProps & {
+    Component: NextPageWithLayout;
+  }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+  return (
+    <GlobalLayout>
+      {getLayout(<Component {...pageProps} />)}
+    </GlobalLayout>
+  );
 }
